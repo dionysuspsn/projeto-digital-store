c70fdcfac20bffcdb462e1431217dbdd5faf8995
Dionisio <dionisiopsn@gmail.com>
2026-01-22 14:18:12 -0300

    ProductCard

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './ProductCard.css';
+
+const ProductCard = ({ image, name, price, priceDiscount }) => {
+  return (
+    <div className="product-card">
+      <div className="card-image">
+        <img src={image} alt={name} />
+      </div>
+      
+      <div className="card-info">
+        <span className="product-category">Tênis</span>
+        <h3 className="product-name">{name}</h3>
+        
+        <div className="price-container">
+          {priceDiscount ? (
+            <>
+              <span className="price-old">R$ {price.toFixed(2)}</span>
+              <span className="price-discount">R$ {priceDiscount.toFixed(2)}</span>
+            </>
+          ) : (
+            <span className="price-regular">R$ {price.toFixed(2)}</span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
